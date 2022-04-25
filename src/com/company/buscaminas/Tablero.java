4cadbdb179510476db9f1d92ba826dbeed31f228
Joan Vallbona <johnyvallbona@gmail.com>
2022-04-25 10:49:30 +0200

    Creat metode per crear caselles

--- src/com/company/buscaminas/Tablero.java
+++ src/com/company/buscaminas/Tablero.java
@@ -7,4 +7,16 @@ public class Tablero {
     public Tablero (){
         this.casellas = new Casella[8][7];
     }
+
+    public Casella[][] getCasellas() {
+        return casellas;
+    }
+
+    public void crearCaselles (Tablero tablero){
+        for (int i = 0; i < tablero.getCasellas().length; i++){
+            for (int j = 0; j < tablero.getCasellas()[0].length; j++){
+                tablero.getCasellas()[i][j] = new Casella(i, j);
+            }
+        }
+    }
 }
