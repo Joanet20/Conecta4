a445997199c3243018191b98f480d6732dcc00e6
Joan Vallbona <johnyvallbona@gmail.com>
2022-04-25 10:34:10 +0200

    Afegit el contructor de Casella i metodes per saber si esta ocupada

--- src/com/company/buscaminas/Casella.java
+++ src/com/company/buscaminas/Casella.java
@@ -6,5 +6,17 @@ public class Casella {
     private int casellaX;
     private int casellaY;
 
+    public Casella (int casellaX, int casellaY){
+        this.ocupada = false;
+        this.casellaX = casellaX;
+        this.casellaY = casellaY;
+    }
 
+    public boolean isOcupada(){
+        return ocupada;
+    }
+
+    public void marcarCasella (boolean ocupada){
+        this.ocupada = true;
+    }
 }
