470dee069472c51fd292105b09bd2fc67df5969f
Joan Vallbona <johnyvallbona@gmail.com>
2022-04-25 10:25:16 +0200

    Creat constructor tablero, atributs X i Y per casella

--- src/com/company/buscaminas/Casella.java
+++ src/com/company/buscaminas/Casella.java
@@ -3,4 +3,8 @@ package com.company.buscaminas;
 public class Casella {
 
     private boolean ocupada;
+    private int casellaX;
+    private int casellaY;
+
+
 }
--- src/com/company/buscaminas/Tablero.java
+++ src/com/company/buscaminas/Tablero.java
@@ -3,4 +3,8 @@ package com.company.buscaminas;
 public class Tablero {
 
     private Casella[][] casellas;
+
+    public Tablero (){
+        this.casellas = new Casella[8][7];
+    }
 }
