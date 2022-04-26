65a47bccffe77931f3e8dfdf32a58fa349d5cd79
Joan Vallbona <johnyvallbona@gmail.com>
2022-04-26 12:34:16 +0200

    Separació de mètodes per controlar si un jugador ha guanyat

--- src/com/company/conecta4/Tablero.java
+++ src/com/company/conecta4/Tablero.java
@@ -24,117 +24,149 @@ public class Tablero {
         for (int i = 0; i < tablero.getCasellas()[1].length; i++){
             for (int j = 0; j < tablero.getCasellas().length; j++){
 
-                if (i >= 3){
-                    if ((tablero.getCasellas()[j][i-1].isOcupada() &&
-                            tablero.getCasellas()[j][i-2].isOcupada() &&
-                            tablero.getCasellas()[j][i-3].isOcupada() &&
-                            tablero.getCasellas()[j][i].isOcupada()) &&
+                comprobaEsquerra(i, j, tablero, jugador);
 
-                            (tablero.getCasellas()[j][i-1].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j][i-2].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j][i-3].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
-                        jugador.setHasGuanyat();
-                    }
-                }
+                comprobarEsquerraAdalt(i, j, tablero, jugador);
 
-                if (i >= 3 && j >= 3){
-                    if (tablero.getCasellas()[j-1][i-1].isOcupada() &&
-                            tablero.getCasellas()[j-2][i-2].isOcupada() &&
-                            tablero.getCasellas()[j-3][i-3].isOcupada() &&
-                            tablero.getCasellas()[j][i].isOcupada() &&
+                comprobaEsquerraAbaix(i, j, tablero, jugador);
 
-                            (tablero.getCasellas()[j-1][i-1].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j-2][i-2].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j-3][i-3].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
-                        jugador.setHasGuanyat();
-                    }
-                }
+                comprobaAdalt(i, j, tablero, jugador);
 
-                if (i >= 3 && (j+3) < tablero.getCasellas().length){
-                    if (tablero.getCasellas()[j+1][i-1].isOcupada() &&
-                            tablero.getCasellas()[j+2][i-2].isOcupada() &&
-                            tablero.getCasellas()[j+3][i-3].isOcupada() &&
-                            tablero.getCasellas()[j][i].isOcupada() &&
+                comprobaAbaix(i, j, tablero, jugador);
 
-                            (tablero.getCasellas()[j+1][i-1].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j+2][i-2].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j+3][i-3].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
-                        jugador.setHasGuanyat();
-                    }
-                }
+                comprobaDreta(i, j, tablero, jugador);
 
-                if (j >= 3){
-                    if (tablero.getCasellas()[j-1][i].isOcupada() &&
-                            tablero.getCasellas()[j-2][i].isOcupada() &&
-                            tablero.getCasellas()[j-3][i].isOcupada() &&
-                            tablero.getCasellas()[j][i].isOcupada() &&
+                comprobaDretaAdalt(i, j, tablero, jugador);
 
-                            (tablero.getCasellas()[j-1][i].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j-2][i].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j-3][i].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
-                        jugador.setHasGuanyat();
-                    }
-                }
+                comprobaDretaAbaix(i, j, tablero, jugador);
+            }
+        }
+    }
 
-                if ((j+3) < tablero.getCasellas().length){
-                    if (tablero.getCasellas()[j+1][i].isOcupada() &&
-                            tablero.getCasellas()[j+2][i].isOcupada() &&
-                            tablero.getCasellas()[j+3][i].isOcupada() &&
-                            tablero.getCasellas()[j][i].isOcupada() &&
+    public void comprobaEsquerra (int i, int j, Tablero tablero, Jugador jugador){
+        if (i >= 3){
+            if ((tablero.getCasellas()[j][i-1].isOcupada() &&
+                    tablero.getCasellas()[j][i-2].isOcupada() &&
+                    tablero.getCasellas()[j][i-3].isOcupada() &&
+                    tablero.getCasellas()[j][i].isOcupada()) &&
 
-                            (tablero.getCasellas()[j+1][i].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j+2][i].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j+3][i].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
-                        jugador.setHasGuanyat();
-                    }
-                }
+                    (tablero.getCasellas()[j][i-1].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j][i-2].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j][i-3].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
+                jugador.setHasGuanyat();
+            }
+        }
+    }
 
-                if ((i+3) < tablero.getCasellas()[1].length){
-                    if (tablero.getCasellas()[j][i+1].isOcupada() &&
-                            tablero.getCasellas()[j][i+2].isOcupada() &&
-                            tablero.getCasellas()[j][i+3].isOcupada() &&
-                            tablero.getCasellas()[j][i].isOcupada() &&
+    public void comprobarEsquerraAdalt (int i, int j, Tablero tablero, Jugador jugador){
+        if (i >= 3 && j >= 3){
+            if (tablero.getCasellas()[j-1][i-1].isOcupada() &&
+                    tablero.getCasellas()[j-2][i-2].isOcupada() &&
+                    tablero.getCasellas()[j-3][i-3].isOcupada() &&
+                    tablero.getCasellas()[j][i].isOcupada() &&
 
-                            (tablero.getCasellas()[j][i+1].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j][i+2].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j][i+3].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
-                        jugador.setHasGuanyat();
-                    }
-                }
+                    (tablero.getCasellas()[j-1][i-1].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j-2][i-2].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j-3][i-3].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
+                jugador.setHasGuanyat();
+            }
+        }
+    }
 
-                if ((i+3) < tablero.getCasellas()[1].length && j >= 3){
-                    if (tablero.getCasellas()[j-1][i+1].isOcupada() &&
-                            tablero.getCasellas()[j-2][i+2].isOcupada() &&
-                            tablero.getCasellas()[j-3][i+3].isOcupada() &&
-                            tablero.getCasellas()[j][i].isOcupada() &&
+    public void comprobaEsquerraAbaix (int i, int j, Tablero tablero, Jugador jugador){
+        if (i >= 3 && (j+3) < tablero.getCasellas().length){
+            if (tablero.getCasellas()[j+1][i-1].isOcupada() &&
+                    tablero.getCasellas()[j+2][i-2].isOcupada() &&
+                    tablero.getCasellas()[j+3][i-3].isOcupada() &&
+                    tablero.getCasellas()[j][i].isOcupada() &&
 
-                            (tablero.getCasellas()[j-1][i+1].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j-2][i+2].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j-3][i+3].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
-                        jugador.setHasGuanyat();
-                    }
-                }
+                    (tablero.getCasellas()[j+1][i-1].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j+2][i-2].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j+3][i-3].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
+                jugador.setHasGuanyat();
+            }
+        }
+    }
 
-                if ((i+3) < tablero.getCasellas()[1].length && (j+3) < tablero.getCasellas().length){
-                    if (tablero.getCasellas()[j+1][i+1].isOcupada() &&
-                            tablero.getCasellas()[j+2][i+2].isOcupada() &&
-                            tablero.getCasellas()[j+3][i+3].isOcupada() &&
-                            tablero.getCasellas()[j][i].isOcupada() &&
+    public void comprobaAdalt (int i, int j, Tablero tablero, Jugador jugador){
+        if (j >= 3){
+            if (tablero.getCasellas()[j-1][i].isOcupada() &&
+                    tablero.getCasellas()[j-2][i].isOcupada() &&
+                    tablero.getCasellas()[j-3][i].isOcupada() &&
+                    tablero.getCasellas()[j][i].isOcupada() &&
 
-                            (tablero.getCasellas()[j+1][i+1].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j+1][i+2].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j+1][i+3].getIdJugador() == jugador.getId() &&
-                                    tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
-                        jugador.setHasGuanyat();
-                    }
-                }
+                    (tablero.getCasellas()[j-1][i].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j-2][i].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j-3][i].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
+                jugador.setHasGuanyat();
+            }
+        }
+    }
+
+    public void comprobaAbaix (int i, int j, Tablero tablero, Jugador jugador){
+        if ((j+3) < tablero.getCasellas().length){
+            if (tablero.getCasellas()[j+1][i].isOcupada() &&
+                    tablero.getCasellas()[j+2][i].isOcupada() &&
+                    tablero.getCasellas()[j+3][i].isOcupada() &&
+                    tablero.getCasellas()[j][i].isOcupada() &&
+
+                    (tablero.getCasellas()[j+1][i].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j+2][i].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j+3][i].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
+                jugador.setHasGuanyat();
+            }
+        }
+    }
+
+    public void comprobaDreta (int i, int j, Tablero tablero, Jugador jugador){
+        if ((i+3) < tablero.getCasellas()[1].length){
+            if (tablero.getCasellas()[j][i+1].isOcupada() &&
+                    tablero.getCasellas()[j][i+2].isOcupada() &&
+                    tablero.getCasellas()[j][i+3].isOcupada() &&
+                    tablero.getCasellas()[j][i].isOcupada() &&
+
+                    (tablero.getCasellas()[j][i+1].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j][i+2].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j][i+3].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
+                jugador.setHasGuanyat();
+            }
+        }
+    }
+
+    public void comprobaDretaAdalt (int i, int j, Tablero tablero, Jugador jugador){
+        if ((i+3) < tablero.getCasellas()[1].length && j >= 3){
+            if (tablero.getCasellas()[j-1][i+1].isOcupada() &&
+                    tablero.getCasellas()[j-2][i+2].isOcupada() &&
+                    tablero.getCasellas()[j-3][i+3].isOcupada() &&
+                    tablero.getCasellas()[j][i].isOcupada() &&
+
+                    (tablero.getCasellas()[j-1][i+1].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j-2][i+2].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j-3][i+3].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
+                jugador.setHasGuanyat();
+            }
+        }
+    }
+
+    public void comprobaDretaAbaix (int i, int j, Tablero tablero, Jugador jugador){
+        if ((i+3) < tablero.getCasellas()[1].length && (j+3) < tablero.getCasellas().length){
+            if (tablero.getCasellas()[j+1][i+1].isOcupada() &&
+                    tablero.getCasellas()[j+2][i+2].isOcupada() &&
+                    tablero.getCasellas()[j+3][i+3].isOcupada() &&
+                    tablero.getCasellas()[j][i].isOcupada() &&
+
+                    (tablero.getCasellas()[j+1][i+1].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j+1][i+2].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j+1][i+3].getIdJugador() == jugador.getId() &&
+                            tablero.getCasellas()[j][i].getIdJugador() == jugador.getId())){
+                jugador.setHasGuanyat();
             }
         }
     }
